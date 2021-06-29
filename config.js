837da5abaa6f16b458d97ace5c19274fc71ad08d
unknown <tyghu105@gmail.com>
2021-06-29 21:28:54 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Gửi bạn yêu',
+    introDesc: `Nay ngày lành tháng tốt tao làm cái web chúc mừng sinh nhật bạn!`,
+    btnIntro: 'Cảm ơn bạn Hùng đẹp trai',
+    title: 'Chúc mừng sinh nhật tuổi 19! Thanh xuân vui vẻ nhé 😙',
+    desc: 'Hôm nay trời đẹp tế nhở, Ỏoooooooooooo. Cảm ơn tao đeee',
+    btnYes: 'Cảm ơn bạn Hùng đz lần nữa nhá <3',
+    btnNo: 'Buồi.. Cút... Im',
+    question:'Có gì muốn nói thì cứ ghi vô đây nhé ,có điều bất ngờ đấy =))',
+    btnReply: 'Bấm để tiếp tục điều thú zị',
+    reply: 'Tao tính tháng sau chia tay con Uyên mày ạ :<                ',
+    mess: 'Snvv & Chúc giảm được cân 🥰. Cảm ơn vì đã xem hết 😘😘',
+    messDesc: 'Click nốt cái dưới cho tôi vui nhé bạn!!!',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/messages/t/100015635537621'
+}
